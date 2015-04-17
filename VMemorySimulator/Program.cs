82d618e013aad8ff7aa3eed5b0dd1a805102833b
Leonardo Brito <leobri93@gmail.com>
2015-04-16 23:15:59 -0300

    First debug commit and test

--- VMemorySimulator/Program.cs
+++ VMemorySimulator/Program.cs
@@ -14,6 +14,7 @@ namespace VMemorySimulator
         [STAThread]
         static void Main()
         {
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
